97d521a317088114b20e4b806ce23237dbda099d
Vitor Costa <vitorcostaqz@gmail.com>
2025-06-11 12:18:21 -0300

    Inicio_Projeto (CSS Quase completo. Inicio JS)

---
+++ Venda_Automoveis/src/js/scripts.js
@@ -0,0 +1,21 @@
+const carros = [
+  {
+    nome: "Chevrolet Opala 1978",
+    preco: 30000,
+    data: "1978-01-01",
+    imagem: "imagens/opala.jpg"
+  },
+  {
+    nome: "VW Fusca 1965",
+    preco: 18000,
+    data: "1965-05-20",
+    imagem: "imagens/fusca.jpg"
+  },
+  {
+    nome: "Ford Maverick 1973",
+    preco: 40000,
+    data: "1973-08-12",
+    imagem: "imagens/maverick.jpg"
+  }
+];
+
